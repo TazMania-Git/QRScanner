fb92cd0fb78ecee230cb8635733479a46ac76745
TazMania <nallinon@gmail.com>
2021-11-17 10:29:56 -0300

    first new commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:qr_scanner/pages/home_screen.dart';
+import 'package:qr_scanner/pages/mapa_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      initialRoute: 'home',
+      routes: {
+        'home':(_)=> HomeScreen(),
+        'mapa':(_)=> MapaScreen(),
+      },
+    );
+  }
+}
